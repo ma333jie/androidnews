89214a2663e1831666a142734aa149c1d9c3b6ea
Jie Ma <ma333jie@berkeley.edu>
2020-04-18 17:08:43 -0700

    ignore api key

--- app/src/main/java/com/laioffer/tinnews/network/ApiKey.java
+++
@@ -1,9 +0,0 @@
-package com.laioffer.tinnews.network;
-
-
-class ApiKey {
-    private static final String API_KEY = "3c5ea640adc545d3b75c31dfe47455e6";
-    public static String getKey(){
-        return API_KEY;
-    }
-}
